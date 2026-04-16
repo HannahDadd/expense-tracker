2b8443e14962ff371ddf35b2be25c1330c2830f8
hannahdadd <hannahbdadd@googlemail.com>
2026-04-16 17:14:09 +0100

    expenses report file structure

--- expenses.js
+++ expenses.js
@@ -1,4 +1,6 @@
 import * as fs from 'fs';
+import { addExpense } from './addExpense.js';
+import { generateReport } from './generateReport.js';
 
 const args = process.argv.slice(2);
 
@@ -30,51 +32,6 @@ function parseArgs(argsArray) {
 }
 
 const { command, options } = parseArgs(args);
-
-function addExpense({ amount, category, note }) {
-
-  // create expense
-  const expense = {
-    id: crypto.randomUUID().slice(0, 8),
-    amount: parseFloat(amount ?? 0),
-    category: category ?? "uncategorized",
-    note: note ?? "UNKNOWN",
-    date: new Date().toISOString()
-  };
-
-  // append expense to file
-  console.log('Adding expense:', expense);
-  let expensesjson = fs.readFileSync("expenses.json","utf-8");
-  let expenses = JSON.parse(expensesjson);
-  
-  if (Object.keys(expenses).length === 0) {
-    expenses = [];
-  }
-
-  expenses.push(expense);
-  fs.writeFile("expenses.json", JSON.stringify(expenses), function (err) {
-    if (err) console.log('Error adding expense:', expense);
-    console.log('Saved!');
-  });
-}
-
-function generateReport({ category }) {
-  console.log('Generating report...');
-
-  fs.readFile("expenses.json", "utf-8", function (err, data) {
-    if (err) console.log('Error generating report:', err);
-
-    const expenses = JSON.parse(data);
-
-    if (category) {
-      console.log(`Filtering by category: ${category}`);
-      const filtered = expenses.filter(e => e.category === category);
-      console.table(filtered);
-    } else {
-      console.table(expenses);
-    }
-  });
-}
 
 switch (command) {
   case 'add':
